7285da4cf358a0eb675d3cdab7573acc0dc824c6
zahrakargar76 <148703264+zahrakargar76@users.noreply.github.com>
2023-11-18 22:49:18 +0330

    Add files via upload

---
+++ Identification.ino
@@ -0,0 +1,56 @@
+/* @file CustomKeypad.pde
+  || @version 1.0
+  || @author Alexander Brevig
+  || @contact alexanderbrevig@gmail.com
+  ||
+  || @description
+  || | Demonstrates changing the keypad size and key values.
+  || #
+*/
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+float firstNumber = 0;
+float secondNumber = 0;
+float result = 0;
+bool section = false;
+int type = 0;
+int led=13;
+//define the cymbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
+
+//initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  char key = customKeypad.getKey();
+  if (key) {
+    static String EnterCode = "";
+    if (key == '#') {
+      if (EnterCode.equals("220")) {
+        digitalWrite(led, HIGH);
+      }
+      else {
+        digitalWrite(led, LOW);
+      }
+      EnterCode = "";
+    }
+    else {
+      EnterCode += key;
+    }
+  }
+}
